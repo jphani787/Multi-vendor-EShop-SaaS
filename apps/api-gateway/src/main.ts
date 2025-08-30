740040e5241b6fd0b19eeedc547a86730ed87bb9
jphani787 <jphanindra787@gmail.com>
2025-08-31 01:48:58 +0530

    changed port for auth and api gateway services

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,19 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import express from 'express';
-import * as path from 'path';
+import express from "express";
+import * as path from "path";
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-gateway!' });
+app.get("/api", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
